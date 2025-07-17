c9fd7b22f500c2ad2a49e84278d15f896dcc0002
MdFawjulAzim <mdfawjulazim617@gmail.com>
2025-07-17 17:04:02 +0600

    Update api.php

--- TicketSystem/routes/api.php
+++ TicketSystem/routes/api.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/profile', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
