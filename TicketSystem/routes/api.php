8b96015ff4dba8b93c79deaeae9e9b425d0f6b88
MdFawjulAzim <mdfawjulazim617@gmail.com>
2025-07-17 16:36:14 +0600

    PreFix Route Removed

--- TicketSystem/routes/api.php
+++ TicketSystem/routes/api.php
@@ -27,50 +27,49 @@ Route::get('/event/{id}', [EventController::class, 'show']);
 Route::middleware(['auth:api'])->group(function () {
 
     // ✅ Admin Only Permissions
-    Route::prefix('admin')->group(function () {
-        Route::get('/dashboard', [AdminController::class, 'dashboard'])
-            ->middleware('permission:view admin dashboard');
 
-        // User Manage
-        Route::get('/user', [AdminController::class, 'index'])->middleware('permission:manage users admin');
-        Route::get('/user/{id}', [AdminController::class, 'show'])->middleware('permission:manage users admin');
-        Route::post('/users', [AdminController::class, 'store'])->middleware('permission:manage users admin');
-        Route::patch('/users/{id}', [AdminController::class, 'update'])->middleware('permission:manage users admin');
-        Route::delete('/users/{id}', [AdminController::class, 'destroy'])->middleware('permission:manage users admin');
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])
+        ->middleware('permission:view admin dashboard');
 
-        // Ticket Manage
-        Route::get('/ticket', [TicketController::class, 'index'])->middleware('permission:manage tickets admin');
-        Route::patch('/ticket/{id}', [TicketController::class, 'update'])->middleware('permission:manage tickets admin');
-        Route::delete('/ticket/{id}', [TicketController::class, 'destroy'])->middleware('permission:manage tickets admin');
+    // User Manage
+    Route::get('/user', [AdminController::class, 'index'])->middleware('permission:manage users admin');
+    Route::get('/user/{id}', [AdminController::class, 'show'])->middleware('permission:manage users admin');
+    Route::post('/users', [AdminController::class, 'store'])->middleware('permission:manage users admin');
+    Route::patch('/users/{id}', [AdminController::class, 'update'])->middleware('permission:manage users admin');
+    Route::delete('/users/{id}', [AdminController::class, 'destroy'])->middleware('permission:manage users admin');
 
-        // Category Manage
-        Route::post('/categories', [CategoryController::class, 'store'])->middleware('permission:create categories admin');
-        Route::patch('/categories/{id}', [CategoryController::class, 'update'])->middleware('permission:edit categories admin');
-        Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->middleware('permission:delete categories admin');
+    // Ticket Manage
+    Route::get('/ticket', [TicketController::class, 'index'])->middleware('permission:manage tickets admin');
+    Route::patch('/ticket/{id}', [TicketController::class, 'update'])->middleware('permission:manage tickets admin');
+    Route::delete('/ticket/{id}', [TicketController::class, 'destroy'])->middleware('permission:manage tickets admin');
 
-        Route::get('/categories', [CategoryController::class, 'index'])->middleware('permission:view categories admin');
-        Route::get('/categories/{id}', [CategoryController::class, 'show'])->middleware('permission:delete categories admin');
+    // Category Manage
+    Route::post('/categories', [CategoryController::class, 'store'])->middleware('permission:create categories admin');
+    Route::patch('/categories/{id}', [CategoryController::class, 'update'])->middleware('permission:edit categories admin');
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->middleware('permission:delete categories admin');
 
-        Route::get('/event', [EventController::class, 'index'])->middleware('permission:view events admin');
-    });
+    Route::get('/categories', [CategoryController::class, 'index'])->middleware('permission:view categories admin');
+    Route::get('/categories/{id}', [CategoryController::class, 'show'])->middleware('permission:delete categories admin');
+
+    Route::get('/event', [EventController::class, 'index'])->middleware('permission:view events admin');
+
 
     // ✅ Organizer Permissions
-    Route::prefix('organizer')->group(function () {
-        Route::get('/dashboard', [OrganizerController::class, 'dashboard'])
-            ->middleware('permission:view dashboard');
 
-        Route::post('/event', [EventController::class, 'store'])->middleware('permission:create events organizer');
-        Route::get('/events', [EventController::class, 'myEvent'])->middleware('permission:view events organizer');
-        Route::patch('/event/{id}', [EventController::class, 'update'])->middleware('permission:edit events organizer');
-        Route::delete('/event/{id}', [EventController::class, 'destroy'])->middleware('permission:delete events organizer');
-    });
+    Route::get('/dashboard', [OrganizerController::class, 'dashboard'])
+        ->middleware('permission:view dashboard');
+
+    Route::post('/event', [EventController::class, 'store'])->middleware('permission:create events organizer');
+    Route::get('/events', [EventController::class, 'myEvent'])->middleware('permission:view events organizer');
+    Route::patch('/event/{id}', [EventController::class, 'update'])->middleware('permission:edit events organizer');
+    Route::delete('/event/{id}', [EventController::class, 'destroy'])->middleware('permission:delete events organizer');
+
 
     // ✅ User Permissions
-    Route::prefix('user')->group(function () {
-        Route::get('/dashboard', [UserController::class, 'dashboard'])
-            ->middleware('permission:view user dashboard',);
 
-        Route::post('/ticket', [TicketController::class, 'store'])->middleware('permission:create ticket user');
-        Route::get('/tickets', [TicketController::class, 'myTickets'])->middleware('permission:View ticket user');
-    });
+    Route::get('/dashboard', [UserController::class, 'dashboard'])
+        ->middleware('permission:view user dashboard',);
+
+    Route::post('/ticket', [TicketController::class, 'store'])->middleware('permission:create ticket user');
+    Route::get('/tickets', [TicketController::class, 'myTickets'])->middleware('permission:View ticket user');
 });
