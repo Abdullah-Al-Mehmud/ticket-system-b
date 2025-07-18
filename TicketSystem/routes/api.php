7f74de35a3c66e384266f475dae25669b3b26dfd
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-07-18 22:36:14 +0600

    Update api.php

--- TicketSystem/routes/api.php
+++ TicketSystem/routes/api.php
@@ -51,7 +51,7 @@ Route::middleware(['auth:api'])->group(function () {
     Route::get('/categories', [CategoryController::class, 'index'])->middleware('permission:view categories admin');
     Route::get('/categories/{id}', [CategoryController::class, 'show'])->middleware('permission:delete categories admin');
 
-    Route::get('/event', [EventController::class, 'index'])->middleware('permission:view events admin');
+    // Route::get('/event', [EventController::class, 'index'])->middleware('permission:view events admin');
 
 
     // ✅ Organizer Permissions
