65544f89b9b2c598ac91e10e258193393e199789
MdFawjulAzim <mdfawjulazim617@gmail.com>
2025-07-27 16:49:43 +0600

    Query OrderBy Change

--- TicketSystem/database/seeders/DatabaseSeeder.php
+++ TicketSystem/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             EventSeeder::class,
             RolePermissionSeeder::class,
             TicketCategorySeeder::class,
-            TicketSeeder::class,
+            // TicketSeeder::class,
         ]);
     }
 }
