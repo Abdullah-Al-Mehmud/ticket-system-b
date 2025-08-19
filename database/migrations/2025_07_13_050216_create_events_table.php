d9c54945a0f19f860162b0a155663e8f57baa411
MdFawjulAzim <mdfawjulazim617@gmail.com>
2025-08-19 14:48:43 +0600

    Create UUID Category Table

--- database/migrations/2025_07_13_050216_create_events_table.php
+++ database/migrations/2025_07_13_050216_create_events_table.php
@@ -13,7 +13,8 @@ return new class extends Migration {
         Schema::create('events', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->uuid('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->string('title');
             $table->text('event_description');
             $table->string('location');
